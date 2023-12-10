79c3e05afe064d3ceaddfd193fa45c031f566d32
Siva Nithiya Sree.C <csivanithiyasree@gmail.com>
2023-12-10 19:03:45 +0530

    changes updated

---
+++ Day2/nithiyapp/src/App.js
@@ -0,0 +1,13 @@
+import './Assets/Css/style.css'
+function App() {
+    return (
+        <div>
+
+            <h1>NITHIYA</h1>
+            <p>HELLO THIS IS NITHIYA</p>
+            Hello World
+        <button type="submit">Login</button>
+        </div>
+        )
+}
+export default App;
